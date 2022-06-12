dd3d1fb32c51757d2843d289ed925b96d2b80ded
Guillermo Vera <veraguillermo345@gmail.com>
2022-06-12 14:09:22 -0300

    Agregando tp 2

---
+++ TP1_IPOO_VeraGuillermo/Viaje.php
@@ -0,0 +1,141 @@
+<?php
+
+class Viaje{
+    //atributos
+    private $codigoViaje;
+    private $destino;
+    private $maximoPasajeros;
+    private $pasajerosActual;
+    private $pasajeros;
+    private $obj_responable_viaje;
+
+    
+    public function __construct($codViaje, $dest, $maxPas, $pasAct, $pasaj, $respV){
+        $this->codigoViaje = $codViaje;
+        $this->destino = $dest;
+        $this->maximoPasajeros = $maxPas;
+        $this->pasajerosActual = $pasAct;
+        $this->pasajeros = $pasaj;
+        $this->obj_responable_viaje = $respV;
+    }
+
+    public function setCodigoViaje($cod){
+        $this->codigoViaje = $cod;
+    }
+
+    public function getCodigoViaje(){
+        return $this->codigoViaje;
+    }
+
+    public function setDestino($d){
+        $this->destino = $d;
+    }
+
+    public function getDestino(){
+        return $this->destino;
+    }
+
+    public function setMaximoPasajeros($max){
+        $this->maximoPasajeros = $max;
+    }
+
+    public function getMaximoPasajeros(){
+        return $this->maximoPasajeros;
+    }
+
+    public function setPasajerosActual($pasA){
+        $this->pasajerosActual = $pasA;
+    }
+
+    public function getPasajerosActual(){
+        return $this->pasajerosActual;
+    }
+
+    public function setPasajeros($pasaj){
+        $this->pasajeros = $pasaj;
+    }
+
+    public function getPasajeros(){
+        return $this->pasajeros;
+    }
+
+    public function setResponsableViaje($respViaje){
+        $this->obj_responable_viaje = $respViaje;
+    }
+
+    public function getResponsableViaje(){
+        return $this->obj_responable_viaje;
+    }
+
+    public function __toString() {
+        return "Codigo de viaje: ".$this->getCodigoViaje().", Destino: ".$this->getDestino().
+        ".\nCantidad maxima de pasajeros: ".$this->getMaximoPasajeros().", Pasajeros actuales en el viaje: ".$this->getPasajerosActual().
+        ".\n".$this->mostrarPasajeros()."\n".$this->getResponsableViaje()->__toString();
+    }
+
+    /** metodo que permite modificar los datos de un pasajero 
+     * @param int $nroDocPas, $doc
+     * @param string $nom, $ape
+     * @return void
+    */
+    public function cambiarDatosUnPasajero($nroDocPas, $nom, $ape, $tel){
+        $arrPasajeros = [];
+        $arrPasajeros = $this->getPasajeros();
+        $cantPasajeros = $this->getPasajerosActual();
+        $i = 0;
+        $bandera = false;
+        $indiceModificar = null;
+        while ($i < $cantPasajeros && $bandera == false){
+            if ($nroDocPas == $arrPasajeros[$i]->getNumeroDocumento()){
+                $bandera = true;
+                $indiceModificar = $i;
+            }
+            $i++;
+        }
+        if ($i <= $cantPasajeros){
+            $arrPasajeros[$indiceModificar]->setNombre($nom);
+            $arrPasajeros[$indiceModificar]->setApellido($ape);
+            $arrPasajeros[$indiceModificar]->setTelefono($tel);
+            $this->setPasajeros($arrPasajeros);
+        } else {
+            echo "\nNo hay un pasajero con ese numero de documento\n";
+        }
+    }
+
+    /** metodo que permite agregar un pasajero al arreglo
+     * @param int $numDoc
+     * @return void
+     */
+    public function agregarPasajero($numDoc, $nom, $ape, $tel){
+        $arrayPasaj = $this->getPasajeros();
+        $cantPasaj = $this->getPasajerosActual();
+        $i = 0;
+        $bandera = false;
+        while ($i < $cantPasaj && $bandera == false) {
+            if ($arrayPasaj[$i]->getNumeroDocumento() == $numDoc ){
+                $bandera = true;
+            }
+            $i++;
+        }
+        if ($i < $cantPasaj){
+            echo "Ese pasajero ya esta registrado.\n";
+        }else{
+            $this->setPasajerosActual($cantPasaj+1);
+            $nuevoPasaj = new Pasajero($nom, $ape, $numDoc, $tel);
+            $arrayPasaj[$i] = $nuevoPasaj;
+            $this->setPasajeros($arrayPasaj);
+        }
+    }
+
+    
+    public function mostrarPasajeros(){
+        $arrayPas = [];
+        $arrayPas = $this->getPasajeros();
+        $countPas = $this->getPasajerosActual();
+        echo "\n--------Datos de pasajeros:--------\n\n";
+        foreach ($this->getPasajeros() as $objPasaj) { 
+            echo $objPasaj->__toString().".\n";
+        }
+    }
+
+}
---
+++ TP1_IPOO_VeraGuillermo/testViaje.php
@@ -0,0 +1,175 @@
+<?php
+include 'Viaje.php';
+include 'Pasajero.php';
+include 'ResponsableV.php';
+
+//instanciacion de objetos Pasajero.
+$pasajero1 = new Pasajero("Jorge", "Gonzales", 13727931, 2944121212);
+$pasajero2 = new Pasajero("Pablo", "Fernandez", 21729034, 2944232323);
+$pasajero3 = new Pasajero("Cristian", "Lopez", 40194852, 2944343434);
+$pasajero4 = new Pasajero("Pablo", "Diaz", 98374263, 2944454545);
+$pasajero5 = new Pasajero("Carlos", "Perez", 13755931, 2944121299);
+$pasajero6 = new Pasajero("Carlos", "Leiva", 32845721, 2944565656);
+$pasajero7 = new Pasajero("Agustina", "Sanchez", 32957843, 2944676767);
+$pasajero8 = new Pasajero("Camila", "Caruso", 33912738, 2944787878);
+$pasajero9 = new Pasajero("Maria", "Banega", 41893857, 2944898989);
+
+//Carga de arreglo de objetos Pasajero.
+$arrayPasajeros = [];
+$arrayPasajeros[0] = $pasajero1;
+$arrayPasajeros[1] = $pasajero2;
+$arrayPasajeros[2] = $pasajero3;
+$arrayPasajeros[3] = $pasajero4;
+$arrayPasajeros[4] = $pasajero5;
+$arrayPasajeros[5] = $pasajero6;
+$arrayPasajeros[6] = $pasajero7;
+$arrayPasajeros[7] = $pasajero8;
+$arrayPasajeros[8] = $pasajero9;
+
+    /* Programa Principal */
+
+        //Instanciacion de objeto Viaje.
+        //****************************************/
+        echo "Desea cargar informacion de un viaje?\n";
+        $resp = trim(fgets(STDIN));
+        if ($resp == "si") {
+
+            echo "Ingrese el codigo de viaje: \n";
+            $codiViaje = trim(fgets(STDIN));
+
+            echo "Ingrese el destino del viaje: \n";
+            $destViaje = trim(fgets(STDIN));
+
+            echo "Ingrese la cantidad maxima de pasajeros: \n";
+            $cantMaxPasajeros = trim(fgets(STDIN));
+
+
+            $cantPasajActual = count($arrayPasajeros);
+            $viaje1 = new Viaje($codiViaje, $destViaje, $cantMaxPasajeros, $cantPasajActual, $arrayPasajeros, "");
+
+        //Switch para ejecutar metodos
+        do {    
+            echo "\n----Menu opciones:------\n\n";
+            echo "1 - modificar datos del viaje\n";
+            echo "2 - modificar datos de un pasajero\n";
+            echo "3 - agregar un pasajero\n";
+            echo "4 - salir del menu\n";
+            $varSwitch = trim(fgets(STDIN));
+            switch ($varSwitch) {
+                case 1:
+                    //Metodo modificarViaje objeto Viaje.
+                    //****************************************/
+                    echo "Desea modificar los datos del viaje? \n";
+                    $respMod = trim(fgets(STDIN));
+                    if ($respMod == "si"){
+
+                        echo "Desea modificar el codigo de viaje? \n";
+                        $respC = trim(fgets(STDIN));
+                        if ($respC == "si"){
+
+                            echo "Ingrese el codigo de viaje nuevo: \n";
+                            $codViajeMod = trim(fgets(STDIN));
+                            $viaje1->setCodigoViaje($codViajeMod);
+
+                        }
+
+                        echo "Desea modificar el destino de viaje? \n";
+                        $respD = trim(fgets(STDIN));
+                        if ($respD == "si"){
+
+                            echo "Ingrese el destino de viaje nuevo: \n";
+                            $destViajeMod = trim(fgets(STDIN));
+                            $viaje1->setDestino($destViajeMod);
+
+                        }
+
+                        echo "Desea modificar la cantidad maxima de pasajeros? \n";
+                        $respCMP = trim(fgets(STDIN));
+                        if ($respCMP == "si"){
+
+                            echo "Ingrese el codigo de viaje nuevo: \n";
+                            $cantMaxPasMod = trim(fgets(STDIN));
+                            $viaje1->setMaximoPasajeros($cantMaxPasMod);
+
+                        }
+
+                    }
+                    break;
+
+                case 2:
+                    //metodo modificarPasajero objeto Pasajero.
+                    //*******************************************/
+                    echo "Desea modificar los datos de un pasajero?\n";
+                    $respModPas = trim(fgets(STDIN));
+                    if ($respModPas == "si" ){
+
+                        echo "Ingrese el numero de documento del pasajero actual para modificar sus datos: \n";
+                        $nroDocPasajero = (trim(fgets(STDIN)));
+
+                        echo "Datos nuevos \n";
+
+                        echo "Ingrese el nombre del pasajero: \n";
+                        $nombreMod = trim(fgets(STDIN));
+
+                        echo "Ingrese el apellido del pasajero: \n";
+                        $apellidoMod = trim(fgets(STDIN));
+
+                        echo "Ingrese el telefono del pasajero: \n";
+                        $telefonoMod = trim(fgets(STDIN));
+
+                        $viaje1->cambiarDatosUnPasajero($nroDocPasajero, $nombreMod, $apellidoMod, $telefonoMod);
+                    }
+                    break;
+
+                case 3:
+                        //Metodo agregarPasajero objeto Viaje.
+                        //***************************** */
+                        echo "Desea agregar un pasajero: \n";
+                        $respAgregarPas = trim(fgets(STDIN));
+                        if ($respAgregarPas == "si"){
+                            echo "Ingrese el numero de documento: \n";
+                            $nroDocumentoNuevo = trim(fgets(STDIN));
+                    
+                                echo "Datos pasajero a agregar \n";
+                    
+                                echo "Ingrese el nombre del pasajero nuevo: \n";
+                                $nombreMod = trim(fgets(STDIN));
+                    
+                                echo "Ingrese el apellido del pasajero nuevo: \n";
+                                $apellidoMod = trim(fgets(STDIN));
+                    
+                                echo "Ingrese el telefono del pasajero nuevo: \n";
+                                $telefonoMod = trim(fgets(STDIN));
+                                
+                                $viaje1->agregarPasajero($nroDocumentoNuevo, $nombreMod, $apellidoMod, $telefonoMod);
+                        }
+
+                    break;
+
+                default:
+                    break;
+            }
+        } while ($varSwitch != 4);
+        
+        //Instanciacion objeto responsable.
+        //***************************** */
+        echo "\n--------Datos del responsable del viaje--------\n\n";
+        echo "Ingrese el numero del numero de empleado: \n";
+        $numEmpleado = trim(fgets(STDIN));
+        echo "Ingrese el numero licencia: \n";
+        $numLicencia = trim(fgets(STDIN));
+        echo "Ingrese el nombre: \n";
+        $nombreEmpleado = trim(fgets(STDIN));
+        echo "Ingrese el apellido: \n";
+        $apellidoEmpleado = trim(fgets(STDIN));
+        $responsable1 = new ResponsableV($numEmpleado, $numLicencia, $nombreEmpleado, $apellidoEmpleado);
+        $viaje1->setResponsableViaje($responsable1);
+
+
+        
+        //Prueba Metodo toString objeto Viaje.
+        //*******************************************/
+        echo "\n--------Datos del viaje: --------\n\n".$viaje1->__toString();
+        echo "\n";
+        
+        }
